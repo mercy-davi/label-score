803f913eef6cb0dc3a5680c43d287c26dd7f8f1a
mercy-davi <hedawei0816@126.com>
2021-04-22 20:01:50 +0800

    initial commit

--- src/test/java/com/example/labelscore/LabelScoreApplicationTest.java
+++ src/test/java/com/example/labelscore/LabelScoreApplicationTest.java
@@ -1,16 +1,11 @@
 package com.example.labelscore;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class LabelScoreApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+public class LabelScoreApplicationTest {
 
 }
