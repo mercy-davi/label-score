b8e2ff10bc6d489a805e01dfbca1d9300f044d93
mercy-davi <hedawei0816@126.com>
2021-04-22 20:32:23 +0800

    initial commit

---
+++ src/main/java/com/example/labelscore/bean/TagView.java
@@ -0,0 +1,11 @@
+package com.example.labelscore.bean;
+
+/**
+ * @Description TODO
+ * @Date 2021/4/3 11:00
+ * @Created by hdw
+ */
+public class TagView {
+    interface Def {}
+    interface instance {}
+}
