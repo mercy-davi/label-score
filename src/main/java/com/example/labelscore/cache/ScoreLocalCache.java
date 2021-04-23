a0ce72029d715e46bab8912ce15e75c8f7033879
mercy-davi <hedawei0816@126.com>
2021-04-23 15:06:39 +0800

    initial commit

---
+++ src/main/java/com/example/labelscore/cache/ScoreLocalCache.java
@@ -0,0 +1,12 @@
+package com.example.labelscore.cache;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description TODO
+ * @Date 2021/4/3 11:16
+ * @Created by hdw
+ */
+@Component
+public class ScoreLocalCache {
+}
