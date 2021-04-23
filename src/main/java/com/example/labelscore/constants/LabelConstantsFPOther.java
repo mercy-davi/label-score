ac43b6d7c4b9b306a901f081a489425879aca319
mercy-davi <hedawei0816@126.com>
2021-04-23 17:55:15 +0800

    initial commit

---
+++ src/main/java/com/example/labelscore/constants/LabelConstantsFPOther.java
@@ -0,0 +1,10 @@
+package com.example.labelscore.constants;
+
+/**
+ * @Classname LabelContantsFPOther
+ * @Description FP子公司差异化常量配置
+ * @Date 2021/4/9
+ * @Author hdw
+ */
+public class LabelConstantsFPOther extends LabelConstants{
+}
