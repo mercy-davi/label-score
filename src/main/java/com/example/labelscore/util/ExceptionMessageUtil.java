7ef522c0d57685dc9bcaa5b617dccdfdba32c747
mercy-davi <hedawei0816@126.com>
2021-06-03 14:07:05 +0800

    initial commit

---
+++ src/main/java/com/example/labelscore/util/ExceptionMessageUtil.java
@@ -0,0 +1,51 @@
+package com.example.labelscore.util;
+
+import com.example.labelscore.vo.ResultVO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Classname ExceptionMessageUtil
+ * @Description TODO
+ * @Date 2021/4/9
+ * @Author hdw
+ */
+public class ExceptionMessageUtil {
+
+    /**
+     * 返回异常信息String
+     * @param e 异常
+     * @return String
+     */
+    public static String errorMessage(Exception e) {
+        StackTraceElement element = e.getStackTrace()[0];
+        return  "异常类: "
+                + element.getFileName()
+                + ", 第"
+                + element.getLineNumber()
+                + "行, 异常信息: " + e.getMessage()
+                + ", 异常类型: " + e.getClass().getName();
+    }
+
+    /**
+     *
+     * @param e 异常
+     * @param message 异常返回的中文信息描述
+     * @return ResultVO
+     */
+    public static ResultVO errResultVO(Exception e, String message) {
+        StackTraceElement element = e.getStackTrace()[0];
+        Map<String, Object> extData = new HashMap<String, Object>() {
+            {
+                put("fileName", element.getFileName());
+                put("lineNumber", element.getLineNumber());
+            }
+        };
+        ResultVO resultVO = ResultVO.fail(message);
+        resultVO.setData(e.toString());
+        resultVO.setExtData(extData);
+
+        return resultVO;
+    }
+}
